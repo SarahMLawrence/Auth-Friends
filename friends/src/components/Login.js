708247c93a6a002774b250e0e1a2cce4a867a692
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-07-09 22:15:20 -0400

    initial commit

---
+++ friends/src/components/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: "",
+    },
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+          />
+
+          <input
+            type="passwor"
+            name="password"
+            value={this.state.credentials.password}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
