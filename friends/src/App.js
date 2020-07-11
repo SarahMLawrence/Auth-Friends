2624624fbe45b51da48e2c1a60ca5571355a29a7
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-07-10 20:23:56 -0400

    makeing App a class

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
 import FriendList from "./components/FriendList";
@@ -8,31 +9,27 @@ import AddFriend from "./components/AddFriend";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <ul>
-          <li>
+class App extends React.Component {
+  state = { credentials: {} };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="nav">
             <Link to="/login">Login</Link>
-          </li>
-          <li>
             <Link to="/protected">Friends List</Link>
-          </li>
-          <li>
             <Link to="/add-friend">Add New Friend</Link>
-          </li>
-        </ul>
+          </div>
 
-        <Switch>
-          <PrivateRoute exact path="/protected" component={FriendList} />
-          <PrivateRoute exact path="/add-friend" component={AddFriend} />
-          <Route path="login" component={Login} />
-          <Route component={Login} />
-        </Switch>
-      </div>
-    </Router>
-  );
+          <Switch>
+            <Route path="login" component={Login} />
+            <PrivateRoute exact path="/protected" component={FriendList} />
+            <PrivateRoute exact path="/add-friend" component={AddFriend} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
